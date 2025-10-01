278da5ef630de23fe6330aaa825bc27debb4ad62
Desarrollo Tibs <cursos.capacitacion@tibs.com.mx>
2025-10-01 01:05:42 -0600

    implementacion de prefijo

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+ app.setGlobalPrefix('api'); 
   // Enable CORS for frontend communication
   app.enableCors();
 
@@ -14,7 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
